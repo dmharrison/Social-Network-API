289d1b50fdbb1afa8b20b2d2b1d09af6c4918a2f
dmharrison <dmharrison2014@gmail.com>
2024-05-24 19:46:09 -0400

    add a friend to the user

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,4 +73,22 @@ module.exports = {
       return res.status(500).json(err);
     }
   },
+  //add a friend to user
+  addFriend: async (req, res) => {
+    try {
+      const result = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { new: true }
+      );
+
+      if (!result) {
+        return res.status(400).json({ message: "User not found" });
+      }
+
+      return res.status(200).json(result);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  },
 };
