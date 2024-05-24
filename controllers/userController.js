ab2a39ff54a36871c4865516869a68d3f2171712
dmharrison <dmharrison2014@gmail.com>
2024-05-24 19:47:19 -0400

    remove a friend to the user

--- controllers/userController.js
+++ controllers/userController.js
@@ -91,4 +91,22 @@ module.exports = {
       return res.status(500).json(err);
     }
   },
+  //remove a friend from user
+  removeFriend: async (req, res) => {
+    try {
+      const result = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+
+      if (!result) {
+        return res.status(400).json({ message: "User not found" });
+      }
+
+      return res.status(200).json(result);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  },
 };
